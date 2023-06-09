31df9916e4a6f395d9521aabe59a4ab1967fc835
iwehf <mh739025250@gmail.com>
2023-06-09 11:19:07 +0800

    remove bot proxy

--- fuo/main.py
+++ fuo/main.py
@@ -1,6 +1,5 @@
 import asyncio
 import logging
-import os
 
 import discord
 from discord.ext import commands
@@ -10,16 +9,12 @@ from fuo import cogs, config, db, log
 _logger = logging.getLogger(__name__)
 
 
-hostip = os.getenv("hostip") or "127.0.0.1"
-proxy = f"http://{hostip}:10081"
-print(f"proxy: {proxy}")
-
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 intents.reactions = True
 
-bot = commands.Bot(command_prefix="%", intents=intents, proxy=proxy)
+bot = commands.Bot(command_prefix="%", intents=intents)
 
 
 @bot.event
