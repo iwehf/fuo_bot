cf3b257d846112a57fedc96364646ce716abb326
iwehf <mh739025250@gmail.com>
2023-06-14 11:43:46 +0800

    add more valid reaction emojis

--- fuo/utils/emoji.py
+++ fuo/utils/emoji.py
@@ -1,11 +1,13 @@
+import logging
 import re
 
 import emoji as em
 
-LIKE_EMOJI_CODES = ["thumbs_up"]
-DISLIKE_EMOJI_CODES = ["cross_mark"]
+LIKE_EMOJI_CODES = ["thumbs_up", "hundred_points", "red_heart", "tears_of_joy"]
+DISLIKE_EMOJI_CODES = ["cross_mark", "neutral_face"]
 VALID_EMOJI_CODES = LIKE_EMOJI_CODES + DISLIKE_EMOJI_CODES
 
+_logger = logging.getLogger(__name__)
 
 def is_valid_emoji(emoji: str):
     if em.is_emoji(emoji):
